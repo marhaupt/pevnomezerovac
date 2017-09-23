75bbe38546c9fdfa3ab9f2a9152768beefc73664
Martin Haupt <martin23haupt@gmail.com>
2017-09-23 21:02:04 +0200

    made it nicer

--- src/spacer.js
+++ src/spacer.js
@@ -14,8 +14,6 @@ function spacer() {
   let output = dealWithSpaces(loadedTextArray);
 
   showFormattedResult(output);
-
-  document.getElementById('nobreakspaced').value = output;
 }
 
 function dealWithSpaces(textInArray) {
